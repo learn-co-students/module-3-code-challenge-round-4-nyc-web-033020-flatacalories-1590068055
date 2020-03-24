39619e8e4a1638f4ebc9197d7b234245944f56ff
vidhisharma3193 <vidhisharma3193@gmail.com>
2020-03-24 11:03:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const USER_ID = 0 // Replace this with the user id given to you by your instructor!
+const BASE_URL = `https://warm-shore-17060.herokuapp.com/api/v1/users/${USER_ID}`
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
