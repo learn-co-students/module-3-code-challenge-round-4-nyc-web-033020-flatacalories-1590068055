725dcc983114d106ec7c5a23508eff9bceaa6df8
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-05-21 11:22:17 -0400

    on second bonus deliverable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+//   MAIN DELIVERABLES
+// [X] list char names in dropdown (get)
+// [X] listener 'change' will display will show char in #detailed-info div
+// [X] form.input.value will replace #calories.textContent upon 'submit'
+
+//   BONUS DELIVERABLES
+// [X] reset calories button function (patch)
+// [] edit chars name (patch)
+// [] add new char (post)
+
+
 URL = 'http://localhost:3000/characters'
 const dropdown = document.querySelector('#character-names')
 const calForm = document.querySelector('#calories-form')
@@ -7,6 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
 fetch(URL)
 .then(res => res.json())
 .then(chars => renderDropdown(chars))
+resetCals()
+createChar()
 })
 
 const renderDropdown = chars => {
@@ -45,10 +58,52 @@ document.addEventListener('submit', e => {
     })
 })
 
+const resetCals = () => {
+    const resetBtn = document.querySelector('#reset-btn')
+    resetBtn.addEventListener('click', e => {
+        charId = calForm.firstElementChild.value
+        calories.textContent = 0
+        fetch(URL + '/' + charId, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                "calories": 0
+            })
+        })
+    })
+}
+
+const editChar = () => {
+    const dropdownDiv = document.querySelector('.dropdown')
+    const newDiv = document.createElement('div')
+    newDiv.style.paddingTop = '20px'
+    newDiv.innerHTML =
+    `
+    <button id="createCharacter">Create New Character</button>
+    `
+    dropdownDiv.append(newDiv)
+    const createBtn = document.querySelector('#createCharacter')
+    createBtn.addEventListener('click', e => {
+
+    })
+}
+
+
+
+
+
+
+
+// THE BELOW CODE IS INTERFERRING WITH THE MAIN DELIVERABLES.
+// MY BEST GUESS IS BECAUSE IT'S LISTENING FOR CLICKS ON THE DOC
+// RESOLVED ISSUE IN resetCals()
 
 // document.addEventListener('click', e => {
 //     if (e.target.id === 'reset-btn')
-//     calories.textContent = 0
+//     
 //     let newCals = calForm.firstElementChild.nextElementSibling.value
 //     calories.textContent = newCals
 //     charId = calForm.firstElementChild.value
@@ -68,9 +123,3 @@ document.addEventListener('submit', e => {
 
 
 
-//   MAIN DELIVERABLES
-// [X] list char names in dropdown (get)
-// [X] listener 'change' will display will show char in #detailed-info div
-// [X] form.input.value will replace #calories.textContent upon 'submit'
-
-//   BONUS DELIVERABLES
