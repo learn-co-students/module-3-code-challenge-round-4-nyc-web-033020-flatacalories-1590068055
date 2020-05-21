25434825bbe04197d7cdaa5e69921afa6f5bf271
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-21 07:42:08 -0700

    displaying

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const charactersUrl = "http://localhost:3000/characters"
 const selectMenu = document.getElementById("character-names")
+const detailedInfoDiv = document.getElementById("detailed-info")
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,9 +27,23 @@ const createCharacterOption = (character) => {
 };
 
 selectMenu.addEventListener('change', (e) => {
-  
   // console.log(e.target)
   // console.log(e.target[e.target.options.selectedIndex].dataset.characterId)
   const characterId = e.target[e.target.options.selectedIndex].dataset.characterId
+  fetch(`${charactersUrl}/${characterId}`)
+  .then(resp => resp.json())
+  .then(addCharacterToDetailedInfo)
 
 });
+
+const addCharacterToDetailedInfo = (character) => {
+  const namePar = document.getElementById('detailed-info')
+  const characterImg = document.getElementById('image')
+  const caloriesSpan = document.getElementById('calories')
+  const formIdInput = document.getElementById('characterId')
+
+  namePar.textContent = character.name
+  characterImg.src = character.image
+  caloriesSpan.textContent = character.calories
+  formIdInput.value = character.id
+};
