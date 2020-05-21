5930e788d24ae2a6388fe80989cd83aebb23e3a5
github username <github email address>
2020-05-21 10:21:53 -0400

    completed 2nd deliverable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* USER STORY
 
-See all character's name in the dropdown menu by **requesting** data from the server
+√ See all character's name in the dropdown menu by **requesting** data from the server
 
 PSEUDO CODE
 
@@ -10,18 +10,28 @@ PSEUDO CODE
 
 USER STORY
 
-Select a character from drop down menu and see character's info inside `#detailed-info` div.
+√ Select a character from drop down menu and see character's info inside `#detailed-info` div.
+
+PSEUDO CODE
 
 √ Get the #detailed-info div, set as const
 √ Add a click listener for on-change
-Set the div display info to the character's info
-    Get char's name
-    Get char's image
-    Get char's total calories (char.calories)
+√ Set the div display info to the character's info
+    √ Get char's name
+    √ Get char's image
+    √ Get char's total calories (char.calories)
 
 USER STORY
 
 Clicks on "Add Calories" button to add calories to a Character. Persist calories value to the server and update the DOM.
+
+PSEUDO CODE
+
+Grab the add calories button and set to a const
+Add event listener to submit button and preventDefault
+Make a fetch('patch') request containing url headers and body info with calorie change
+    The body will contain the input value of the textField
+    Optimistically render the character's new calorie count
 
 
 #####################
@@ -38,12 +48,36 @@ Add a new character
 const url = "http://localhost:3000/characters"
 const dropdown = document.querySelector('#character-names')
 const detailedDiv = document.querySelector('#detailed-info')
-console.log(detailedDiv)
+const caloriesForm = document.querySelector('#calories-form')
+
 
 populateDropdown()
+addIdToSubmitBtn()
+
+function addIdToSubmitBtn(){
+    caloriesForm.children[2].id = "submit"
+}
 
 dropdown.addEventListener('change', event => {
-    
+    let divName = document.querySelector('#name')
+    let divImage = document.querySelector('#image')
+    let divCalories = document.querySelector('#calories')
+
+    changeDisplay()
+
+    function changeDisplay(){
+        fetch(url)
+        .then(resp => resp.json())
+        .then(characters => {
+            characters.forEach(char => {
+                if (char.name === event.target.value){
+                    divName.textContent = char.name
+                    divImage.src = char.image
+                    divCalories.textContent = char.calories 
+                }
+            })
+        })
+    }  
 })
 
 function populateDropdown(){
@@ -54,6 +88,8 @@ function populateDropdown(){
             const charName = document.createElement('option')
             charName.value = char.name
             charName.textContent = char.name
+            charName.dataset.id = char.id
+
             dropdown.appendChild(charName)
         })
     })
