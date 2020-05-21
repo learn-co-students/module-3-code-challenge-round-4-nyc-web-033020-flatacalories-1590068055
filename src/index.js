49ab3cdc9bdffe963b93a6ab578f61b2093a414a
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-05-21 11:46:17 -0400

    got held up on 2nd deliverable and still not working

--- src/index.js
+++ src/index.js
@@ -1,35 +1,60 @@
 /*
-- Fetch request to GET  (/characters) all character's name in the dropdown menu
+- √Fetch request to GET  (/characters) all character's name in the dropdown menu
 - 'Click' listener on the drop down menu to select & see character's info inside #detailed-info div
 - Fetch request to GET (/characters/:id) ind. character
+
+    -Transform drop-down options into an array:
+    const namesArr = Array.from(document.querySelector('#character-names').options)
+
+    - Use forEach on the namesArr to change each character's name/image/calories
+
 - 'Click' listener on "Add Calories" btn to add calories to a Character. Persist calories value to the server and update the DOM
 - Fetch request to PATCH/update (/characters/:id) calorie count
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    charactersURL = 'http://localhost:3000/characters'
+    const charactersURL = 'http://localhost:3000/characters'
+    const allNames = document.querySelector('#character-names')
 
     fetch(charactersURL)
         .then(resp => resp.json())
-        .then(characterNames => characterNames.forEach(charaName => renderNames(charaName)))
+        .then(characters => characters.forEach(character => renderNames(character)))
+       
+    function renderNames(character) {
+            const eachName = document.createElement('option')
+            eachName.innerText = character.name
+            allNames.append(eachName)
 
-    function renderNames(charaName) {
-        const allNames = document.querySelector('#character-names')
-        const eachName = document.createElement('option')
-        console.log(eachName)
-        eachName.innerText = charaName.name
-        allNames.append(eachName)
+            allNames.addEventListener('click', event => {
+                const name = document.querySelector('#name')
+                const image = document.querySelector('#image')
+                const caloriesSpan = document.querySelector('#calories')
+
+                if(event.target.children[1].innerText === 'Mr. Cute') {
+                    console.log(character)
+                    name.textContent = character.name
+                    image.src = character.image
+                    caloriesSpan.textContent = character.calories
+        
+                } 
+            })
     }
 
-    // function renderCharacters(character) {
-    //     const name = document.querySelector('#name')
-    //     const image = document.querySelector('#image')
-    //     const caloriesSpan = document.querySelector('#calories')
+    // fetch(charactersURL/1)
+    //     .then(resp => resp.json())
+    //     .then(character => renderCharacter(character))
 
-    //     name.textContent = character.name
-    //     image.src = character.image
-    //     caloriesSpan.textContent = character.calories
+    // function renderCharacter(character) {
+    //     document.addEventListener('click', event => {
+    //         if(event.target.children[1].innerText === 'Mr. Cute') {
+    //             const name = document.querySelector('#name')
+    //             const image = document.querySelector('#image')
+    //             const caloriesSpan = document.querySelector('#calories')
+    
+    //             name.textContent = character.name //how do I get access to the characters??
+    //             image.src = character.image
+    //             caloriesSpan.textContent = character.calories
+    //         }
+    //     })
     // }
-
-
 })
