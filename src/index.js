35d6c1d8b939a86bb4937269d82c2c89c7131a5b
William <willlin1996@gmail.com>
2020-05-21 10:18:22 -0400

    second save

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const url = "http://localhost:3000/characters"
     const dropdown = document.querySelector('#character-names')
-    console.log(dropdown)
+    const charInfo = document.querySelector("#detailed-info")
     fetch(url)
     .then(response => response.json())
     .then(charData => renderCharacter(charData))
@@ -17,7 +17,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
     function renderCharacter(charData){
         charData.forEach((char)=>{
             let option = document.createElement('option')
+            option.innerText = char.name
             dropdown.appendChild(option)
+        })
+        document.addEventListener('change', function(event){
+            if (event.target === dropdown){
+                charData.forEach((char)=>{
+                    if (event.target.value === char.name){
+                        charInfo.innerHTML = 
+                        `<p id="name">${char.name}</p>
+                        <img id="image${char.id}" src="${char.image}"><!-- display character image here -->
+                        <h4>Total Calories: <span id="calories">${char.calories}</span> </h4>
+                        <form id="calories-form">
+                            <input type="hidden" value="${char.id}" id="${char.id}"/> <!-- Assign character id as a value here -->
+                            <input type="text" placeholder="Enter Calories"/>
+                            <input type="submit" value="Add Calories"/>
+                        </form>
+                        <button id="reset-btn">Reset Calories</button>`
+                    }
+                })
+            }
         })
     }
 })
