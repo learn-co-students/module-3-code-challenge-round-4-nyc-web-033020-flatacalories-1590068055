a01df5fd25a4ee55b4980ffb1881dd384c5872d7
github username <github email address>
2020-05-21 11:37:41 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 
 
+
+// **** Drawing a complete blank here on all of it, I know what I want
+// to do and I have stuff floating around in my head and it feels completely
+// inaccessible and I can't put it together as actual syntax.
+// I don't wan't to make excuses but it's been a hell of a
+// week in my personal life, I'm not exactly on my A game here today.
+// I know I can do it, I could do it in the labs and I understand the material,
+// I felt good about all of it yesterday, I'll just need to do it next week. ****
+
+
+
+// to do:
+
 // fetch characters
 // render characters
 
 // addEventlistener click to see fetched character name in
 // drop down menu
 
-// addEventListener click for fetched details into character details
+// addEventListener click for adding fetched details into character details
 
-// addEventListener click add calories button
+// addEventListener click add calories button, ID in dataset
+// PATCH to update table values, persist new calorie value to DB
 
-// addEventListener click reset calories
+// addEventListener click reset calories, use ID in dataset
 
-// event delegation?? if dropdown, add, reset
+// event delegation?? if button text === dropdown, add, reset
 
 //
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -27,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const renderCharacters = characters => {
     characters.forEach( char => {
-      const
 
     })
   }
@@ -49,4 +65,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-}) //last line
+}) //
