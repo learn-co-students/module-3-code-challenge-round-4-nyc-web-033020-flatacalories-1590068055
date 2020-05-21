2e861995f9b1d34485f8f2429640c665a31e098b
github username <github email address>
2020-05-21 10:42:35 -0400

    3rd commit

--- src/index.js
+++ src/index.js
@@ -9,11 +9,29 @@ document.addEventListener("DOMContentLoaded", function(){
 
     // Render Character Names 
     function renderCharacters(characters) {
-        const characterSelect = document.getElementById("character-names").value 
-        document.getElementById("name").innerHTML = characterSelect 
+        const characterOptions = document.getElementById("character-names") 
         characters.forEach(character => {
-            
+            const characterSelect = document.getElementById("character-names").value 
+            document.getElementById("name").innerHTML = characterSelect 
+            characterSelect.id = character.id 
+            characterSelect.innerText = character.name 
+            characterOptions.append(characterSelect) 
+        }) 
+    } 
+
+    // Listen for dropdown option select. 
+    function characterShow() {
+        const characterOptions = document.getElementById("character-names") 
+        characterOptions.addEventListener("click", e => {
+            const character_id = e.target.id 
+            fetch(`http://localhost:3000/characters/${character_id}`) 
+                .then(resp => resp.json()) 
+                .then(char => renderCharacter(char)) 
         })
-        
+    } 
+
+    function renderCharacter(character) {
+        const characterInfo = document.getElementById("characterInfo") 
+        const detailedInfo = document.getElementById("detailed-info") 
     }
 })
