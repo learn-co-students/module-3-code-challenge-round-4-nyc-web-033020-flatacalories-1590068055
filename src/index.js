ea6d2da50daa895bbf8da057913fb85856b6887c
github username <github email address>
2020-05-21 10:02:18 -0400

    initial

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// See characters name in dropdown menu div.dropdown select#character-names
+// Populate div.characterInfo > div#detailed-info
+// // p#name
+// // img#image
+// // span#calories
+// Add calories to user with form#calories-form
+
+// Advanced Deliverables
+// // Reset calories for user to 0 via button#reset-button
+// // Change Characters name
+// // Add new character
+
+const _HEADERS = {
+	"Content-Type": "application/json",
+	"Accept": "application/json"
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	const dropdown = document.getElementById('character-names');	
+	
+	// GET all characters, pass to populateDropdown
+	const getCharacters = () => {
+		fetch("http://localhost:3000/characters")
+		.then( res => res.json() )
+		.then( characters => populateDropdown(characters) )
+	};
+
+	// Populate dropdown with response from server
+	const populateDropdown = characters => {
+		
+		// Create and populate option element for each character
+		characters.forEach(character => {
+			const characterOption = document.createElement('option');
+
+			characterOption.textContent = character.name;
+			characterOption['data-id'] = character.id;
+
+			dropdown.appendChild(characterOption)
+		});
+	};
+
+	// Create change event listener for dropdown
+	dropdown.addEventListener('change', e => {
+		console.log(e.target.dataset)
+	})
+
+
+	getCharacters();
+})
