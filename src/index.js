96ad1fa35c8b92567ee34f4114050c5b6e277242
Section9-Togusa <corbindac@gmail.com>
2020-05-21 10:42:41 -0400

    3rd Commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const baseURL = "http://localhost:3000"
+    const charactersURL = "http://localhost:3000/characters"
 
     //image 
     const  postName = document.getElementById("name")
     console.log(postName)
     const post = document.getElementById("image")
     console.log(post)
-    const imageLink = "http://localhost:3000/characters"
  
-    fetch(imageLink)
+    fetch(charactersURL)
     .then(resp => resp.json())
     .then(image => {
         renderImage(image)
     })
-    
+
     function renderImage(showImage) {
         post.src = showImage.image
         postName.innerText = showImage.postName
     }
     
+    const addChracter = (characterId) => {
+        console.log(characterId)
+        const object = {
+            method: "POST",
+            headers: {
+                "Content-Type: application/json",
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify({"character_id": characterId})
+        }
+        fetch(charactersURL, object)
+        .then(res => res.json())
+        .then(json => {
+            const option = document.querySelector(`option[data-character-id='${charactersId}']`)
+        })
+
+    }
+
     
     })
     
