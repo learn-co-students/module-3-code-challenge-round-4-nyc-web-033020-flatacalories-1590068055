e6691b71259099f48cf39d72f4007c13083c47ca
github username <github email address>
2020-05-21 11:02:11 -0400

    first deliverable done and part of second

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const url = 'http://localhost:3000/characters'
+    const charUrl = 'http://localhost:3000/characters/'
 
     //make a GET request and render all characters' names in a dropdown menu
     const getCharacters = () => {
@@ -11,10 +12,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const renderCharacters = (characters) => {
         characters.forEach(char => {
+            const select = document.querySelector('#character-names')
+            const newOption = document.createElement('option')
+            newOption.dataset.id = `${char.id}`
+            newOption.text = `${char.name}`
+            select.add(newOption)
+
+            document.addEventListener('change', (e) => {
+                if(e.target.dataset.id === char.id) {
+                    fetch(`${charUrl}/${char.id}`)
+                        .then(resp => resp.json())
+                        .then(renderSingleChar)
+                }
+            })
+        })
+    }
+
+    const renderSingleChar = (char) => {
+        const infoDiv = document.querySelector('#detailed-info')
+        
+    } 
+
+    //add event listener for selecting character from dropdown
+    //make GET request with ID of char and display in detailed info div
+    
 
 
 
+            // const topOption = document.querySelector('#top')
+            // const newOption = document.createElement('option')
+            // newOption.innerHTML +=`
+            // <option value=${char.id}>${char.name}</option>
+            // `
             
+            // topOption.append(newOption)
+
             // const option = document.querySelector('option')
             // const newOption = document.createElement('option')
                       
@@ -22,8 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
             //     <option value='option1'>${char.name}</option>
             //     `
             //     option.append(newOption)
-        })
-    }
+    
 
 
     getCharacters()
