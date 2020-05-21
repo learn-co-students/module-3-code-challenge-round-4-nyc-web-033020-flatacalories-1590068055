0969f06d1298649ca5ac934311a6603bb29c45c2
Section9-Togusa <corbindac@gmail.com>
2020-05-21 11:46:51 -0400

    6th Commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,80 @@
 document.addEventListener('DOMContentLoaded', function() {
     const baseURL = "http://localhost:3000"
     const charactersURL = "http://localhost:3000/characters"
+    const caloiresURL = "http://localhost:3000/characters/calories"
 
-    //image 
-    const  postName = document.getElementById("name")
-    console.log(postName)
-    const post = document.getElementById("image")
-    console.log(post)
+    // //image
+    // const  postName = document.getElementById("name")
+    // console.log(postName)
+    // const post = document.getElementById("image")
+    // console.log(post)
+
+    
  
-    fetch(charactersURL)
-    .then(resp => resp.json())
-    .then(image => {
-        renderImage(image)
+   function fetchImage() {
+       return fetch(charactersURL)
+       .then(res =>j.son)
+   }
+
+   function patchImage(id, data){
+       return fetch(caloiresURL, {
+           method: 'Post',
+           headers: {
+            "Content-Type": "application/json",
+            "Accept": 'application/json'
+           },
+           body: JSON.stringify(data),
+       })
+       .then(res => res.json())
+   }
+
+   function postCalorie(data) {
+    console.log(data)
+    return fetch(caloriesURL, {
+        method:'POST', 
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify(data), 
     })
+    .then(res => res.json())
+}
 
     function renderImage(showImage) {
         const imageShow = document.getElementById('image')
         const imageName = document.getElementById('name')
         const calories = document.getElementById('calories')
         const caloriesForm = document.getElementById('calories-form')
+
+        fetchImage().then(img => {
+            image.show.dataset.id = img.id
+            imageShow.src = img.url 
+            imageName.innerText = img.name
+
+            //calories 
+            img.calories.forEach(calorie => {
+                const li = document.createElement('li')
+                li.id = calorie.id
+                li.innerText = calorie.content
+                calories.append(li)
+            })
+
+            submitBtn.addEventListener('click', () => {
+                event.preventDefault()
+                const calorieInput = document.getElementById("calorie_input").value
+                const caloriedata = {image_id: img.id, content:calorieInput}
+                postCalorie(calorieData)
+                const li = document.createElement('li')
+                li.dataset.id = img.calories.length 
+                li.innerText = calorieInput
+                calories.appendChild(li)
+            })
+
+            
+        })
     }
-    
-    const addChracter = (characterId) => {
+         const addChracter = (characterId) => {
         console.log(characterId)
         const object = {
             method: "POST",
@@ -47,17 +100,15 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }
 
-        function postCalories(data) {
-            console.log(data)
-            return fetch(caloriesURL, {
-                method:'POST', 
-                headers: {
-                    'Accept': 'application/json', 
-                    'Content-Type': 'application/json'
-                }, 
-                body: JSON.stringify(data), 
+            function renderCalories() {
+            const calories = document.getElementById('calories')
+            fetchImage().then(img => {
+                img.calories.forEach(calories => {
+                    const li = document.createElement('li')
+                    li.id = calories.id
+                    li.innerText = calories.content
+                })
             })
-            .then(res => res.json())
         }
 
     }
